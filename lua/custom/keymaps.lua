35d9818f9cbb8ff0283fd52b943fc8d76a5cb393
Kasper Sternberg <kaspersternberg@gmail.com>
2024-10-06 14:09:09 +0200

    Moved code away from init.lua

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,24 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Move lines up and down
 vim.keymap.set('n', 'J', ':m .+1<CR>==')
 vim.keymap.set('n', 'K', ':m .-2<CR>==')
@@ -6,3 +27,6 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 
 -- Reset buffer to saved file with <Leader>r
 vim.keymap.set('n', '<Leader>dr', ':e!<CR>', { desc = '[D]ocument [R]eset' })
+
+-- Toggle line numbers with <leader>tl
+vim.keymap.set('n', '<leader>tl', ToggleLineNumbers, { desc = '[T]oggle [L]ine Numbers' })
