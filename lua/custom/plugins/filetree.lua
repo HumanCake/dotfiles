a187b91613aed0bb570a947704ca2d11877bc376
Kasper Sternberg <kaspersternberg@gmail.com>
2025-03-28 18:53:12 +0100

    Show hidden files in file explorer by default

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,6 +1,3 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -16,11 +13,17 @@ return {
           ['<space>'] = 'none',
         },
       },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        },
+      },
     }
 
     -- Toggle Neo-tree on startup
     vim.cmd [[
       autocmd VimEnter * Neotree toggle last
+
     ]]
 
     -- Keymap for manually toggling Neo-tree
