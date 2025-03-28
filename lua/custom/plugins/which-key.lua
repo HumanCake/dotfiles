428b2ce6cbb629ceae6c5672040d1798f5722b6b
Kasper Sternberg <kaspersternberg@gmail.com>
2025-03-28 19:44:29 +0100

    Fix missing dependency

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,5 +1,6 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
+  dependencies = { 'echasnovski/mini.icons' },
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
     local wk = require 'which-key'
