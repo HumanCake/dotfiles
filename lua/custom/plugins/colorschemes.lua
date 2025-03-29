44206b4a914fe743f3d3597806b0417fd04ac4c9
Kasper Sternberg <kaspersternberg@gmail.com>
2025-03-29 12:32:10 +0100

    Added dracula colorscheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,5 +1,6 @@
 return {
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1001 },
+  { 'Mofiqul/dracula.nvim' },
   {
     'folke/tokyonight.nvim',
     priority = 1000,
@@ -7,7 +8,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
